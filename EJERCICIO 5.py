0065d45758e4d4a02199eef1a503ab4d114231e9
AEIL2203 <140118965+AEIL2203@users.noreply.github.com>
2026-03-01 18:32:29 -0600

    CORRECCIONES

--- EJERCICIO 5.py
+++ EJERCICIO 5.py
@@ -1,8 +1,8 @@
-# Solicitud al usuario una cadena de texto
+#Solicitud al usuario una cadena de texto
 texto = input("Ingrese una cadena: ")
 
-# Se invierte el texto
+#Se invierte el texto
 invertido = texto[::-1]
 
-# Mostramos el resultado
+#Mostramos el resultado
 print("Cadena invertida:", invertido)
